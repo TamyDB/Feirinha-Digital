1cf7a233b34ba167f38f0307e4ffc3dc47c81df5
Rafael Tamy <rafaeltamydb@gmail.com>
2025-09-15 19:36:01 -0300

    feat: creation of main.php to be the shop main page and its early design
    Co-authored-by: KenzoVictor <KenzoVictor@users.noreply.github.com>

--- components/form.php
+++ components/form.php
@@ -5,7 +5,7 @@ if ($form === 'login'): ?>
     <!-- Login Form -->
     <div class="mt-12">
         <h2 class="text-2xl font-bold mb-4">Bem-vindo de volta!</h2>
-        <form class="flex flex-col gap-2 mt-8" action="cadastro.php">
+        <form action="main.php" class="flex flex-col gap-2 mt-8" action="cadastro.php">
             <label for="email">Login:</label>
             <input class="border w-full rounded-sm p-2" name="email" type="text" placeholder="Digite seu email ou usuário">
             <label>Senha:</label>
@@ -16,16 +16,16 @@ if ($form === 'login'): ?>
     </div>
 <?php else: ?>
     <!-- Registro Form -->
-    <div class="mt-12">
+    <div>
         <h2 class="text-2xl font-bold mb-4">Criar nova conta</h2>
-        <form class="flex flex-col gap-2" action="cadastrar.php">
+        <form class="flex flex-col gap-2" action="main.php">
             <label for="email">Email</label>
             <input class="border w-full rounded-sm p-2" name="email" type="text" placeholder="Digite seu email ou usuário">
             <label>Senha:</label>
             <input class="border w-full rounded-sm p-2" type="text" placeholder="Digite sua senha">
             <label>Confirme sua senha:</label>
             <input class="border w-full rounded-sm p-2 mb-2" type="text" placeholder="Digite sua senha novamente">
-            <button class="border p-2 rounded-md g-sky-500 hover:bg-lime-500" type="submit">Cadastrar</button>
+            <button class="border p-2 rounded-md g-sky-500 hover:bg-lime-500 cursor-pointer" type="submit">Cadastrar</button>
         </form>
     </div>
 <?php endif; ?>
